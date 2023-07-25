d4532523db2bec473864b657e5d96f315a797867
IreneMakonnen <irenemakonnen@gmail.com>
2023-07-25 17:27:18 +0300

    Updated hexadecimal file

--- print_hex.c
+++ print_hex.c
@@ -9,11 +9,11 @@
 int print_hexadecimal(char c)
 {
 	char hex[3];
-	unsigned char asc_code = (unsigned char)c;
+	unsigned char ascii = (unsigned char)c;
 
-	hex[0] = ((asc_code / 16) < 10 ? (asc_code / 16) + '0' : (asc_code / 16) - 10 + 'A');
-	hex[1] = ((asc_code % 16) < 10 ? (asc_code % 16) + '0' : (asc_code % 16) - 10 + 'A');
+	hex[0] = (ascii / 16) < 10 ? (ascii / 16) + '0' : (ascii / 16) - 10 + 'A';
+	hex[1] = (ascii % 16) < 10 ? (ascii % 16) + '0' : (ascii % 16) - 10 + 'A';
 	hex[2] = '\0';
 
-	return printf("\\x%s", hex);
+	return (printf("\\x%s", hex));
 }
