eed8b9336e2712eeaffc8e9661d02e36a9b4d962
Dana-Codes <codingbydaniel@gmail.com>
2023-07-26 10:36:11 -0700

    allow

--- printf.c
+++ printf.c
@@ -2,57 +2,82 @@
 #include <stdarg.h>
 #include "main.h"
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    int char_count = 0;
-
-    while (*format) {
-        if (*format == '%') {
-            format++; // Move to the next character after '%'
-            switch (*format) {
-                case 'c': {
-                    char ch = (char)va_arg(args, int);
-                    putchar(ch);
-                    char_count++;
-                    break;
-                }
-                case 's': {
-                    char *str = va_arg(args, char *);
-                    while (*str) {
-                        putchar(*str);
-                        str++;
-                        char_count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    putchar('%');
-                    char_count++;
-                    break;
-                }
-                default:
-                    // Unsupported conversion specifier, just print the '%'
-                    putchar('%');
-                    char_count++;
-                    break;
-            }
-        } else {
-            putchar(*format);
-            char_count++;
-        }
-        format++;
-    }
-
-    va_end(args);
-    return char_count;
+/**
+ * print_char - Helper function to print a single character
+ */
+static int print_char(char c)
+{
+	putchar(c);
+	return (1);
 }
 
-int main() {
-    _printf("Hello, %s!\n", "world");
-    _printf("Character: %c\n", 'A');
-    _printf("Let's print a percent sign: %%\n");
-    return 0;
+/**
+ * print_string - Helper function to print a string
+ */
+static int print_string(char *str)
+{
+	int printed_chars = 0;
+	while (*str != '\0')
+	{
+		putchar(*str);
+		printed_chars++;
+		str++;
+	}
+	return (printed_chars);
+}
+
+/**
+ * _printf - Custom printf function that handles conversion specifiers: c, s, and %
+ *
+ * @format: The format string
+ * Return: Number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int printed_chars = 0;
+	char c;
+	char *str;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				c = (char)va_arg(args, int);
+				printed_chars += print_char(c);
+				break;
+
+			case 's':
+				str = va_arg(args, char*);
+				printed_chars += print_string(str);
+				break;
+
+			case '%':
+				printed_chars += print_char('%');
+				break;
+
+			default:
+				printed_chars += print_char('%');
+				printed_chars += print_char(*format);
+				break;
+			}
+		}
+		else
+		{
+			printed_chars += print_char(*format);
+		}
+
+		format++;
+	}
+
+	va_end(args);
+
+	return (printed_chars);
 }
 
